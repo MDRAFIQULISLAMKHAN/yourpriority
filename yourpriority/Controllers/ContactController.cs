2dec9f8b5a1058398ac908ed1a7e8f09cd9bd0a5
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2021-08-31 18:10:17 +0600

    Contact Form

--- yourpriority/Controllers/ContactController.cs
+++ yourpriority/Controllers/ContactController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using yourpriority.Models;
 
 namespace yourpriority.Controllers
 {
@@ -13,5 +19,57 @@ namespace yourpriority.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult MaiResult(Contact pv)
+        {
+
+            var jsonString = JsonConvert.SerializeObject(pv);
+            pv = JsonConvert.DeserializeObject<Contact>(jsonString);
+
+            try
+            {
+                string body = string.Empty;
+                body += "Hi Team,";
+                body += "<br/> This is from ByteHeart Contact us enquiry:";
+                body += "<br/>Name: " + pv.Name;
+                body += "<br/>Email: " + pv.Email;
+                body += "<br/>Phone: " + pv.Phone;
+                body += "<br/>Office: " + pv.Office;
+                body += "<br/>Refference: " + pv.Refference;
+                body += "<br/>Message: " + pv.Message;
+                body += "<br/> Date: " + DateTime.Now.ToString("dd MMM yyyy");
+
+                bool hasWords = HasBadWords(pv.Message);
+
+                if (hasWords == false)
+                {
+                    var client = new SmtpClient("smtp.gmail.com", 587)
+                    {
+                        Credentials = new NetworkCredential("test.sgff@gmail.com", "testingg"),
+                        EnableSsl = true
+                    };
+                    client.Send("test.sgff@gmail.com", "sagorkhan.fts@gmail.com", "test", body);
+                }
+            }
+            catch (Exception ex)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("<div class=\"alert alert-success\" id=\"contactSuccess\">");
+                sb.AppendFormat(" <strong>Error!</strong> There was an error sending your message.{0} - {1}", ex.Message, ex.InnerException?.Message);
+                sb.AppendFormat("<span class=\"font-size-xs mt-sm display-block\" id=\"mailErrorMessage\"></span>");
+                sb.AppendFormat("</div>");
+                pv.EmailStatus = sb.ToString();
+            }
+
+            return View("~/Views/Contact/Index.cshtml",pv);
+        }
+        public bool HasBadWords(string inputWords)
+        {
+            Regex wordFilter = new Regex("(eаrn mоnеy оnlinе|mystrikingly|makemoney|earnmoney|sеx|adult|fucк|dating|woman|girls)");
+            return wordFilter.IsMatch(inputWords.ToLower());
+        }
+
+       
     }
 }
---
+++ yourpriority/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yourpriority.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Regarding { get; set; }
+        public string Office { get; set; }
+        public string Refference { get; set; }
+        public string Message { get; set; }
+        public string EmailStatus { get; set; }
+    }
+}
