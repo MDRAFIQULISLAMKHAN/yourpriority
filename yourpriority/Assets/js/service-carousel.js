a68dee8d63aa8d878cfbfc8b77dfcb4324ca2b1f
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2021-08-31 10:08:38 +0600

    asdas

--- yourpriority/Assets/js/service-carousel.js
+++ yourpriority/Assets/js/service-carousel.js
@@ -1,6 +1,6 @@
 // jQuery script for service carousel
 
-$(document).on('ready', function() {
+$(document).on('ready', function () {
     "use strict";
     $(".service").owlCarousel({
 
@@ -8,6 +8,36 @@ $(document).on('ready', function() {
         navigation: true, // Show next and prev buttons
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        pagination: false
+
+    });
+
+});// jQuery script for service carousel
+
+$(document).on('ready', function () {
+    "use strict";
+    $(".testimonial").owlCarousel({
+
+        autoPlay: 5000, //Set AutoPlay to 3 seconds
+        navigation: true, // Show next and prev buttons
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        pagination: false
+
+    });
+
+});
+$(document).on('ready', function () {
+    "use strict";
+    $(".logo-slider").owlCarousel({
+
+        autoPlay: 4000, //Set AutoPlay to 3 seconds
+        navigation: false, // Show next and prev buttons
+        navigationText: false,
         itemsDesktop: [1199, 3],
         itemsDesktopSmall: [979, 3],
         pagination: false
