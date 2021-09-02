4ae0a9e6058c4658b3ec7629fbf194cac648c2bb
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2021-09-02 16:18:47 +0600

    dsfs

--- yourpriority/Controllers/ContactController.cs
+++ yourpriority/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -24,8 +25,51 @@ namespace yourpriority.Controllers
         public ActionResult MaiResult(Contact pv)
         {
 
-            var jsonString = JsonConvert.SerializeObject(pv);
-            pv = JsonConvert.DeserializeObject<Contact>(jsonString);
+            /*string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("~/Views/Email_Templates/User_success_email.cshtml")))
+            {
+                body = reader.ReadToEnd();
+            }
+            bool hasWords = HasBadWords(pv.Message);
+
+            if (hasWords == false)
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("test.sgff@gmail.com", "testingg"),
+                    EnableSsl = true
+                };
+                client.Send("test.sgff@gmail.com", "sagorkhan.fts@gmail.com", "test", body);
+            }
+            
+            body = body.Replace("{Name}", pv.Name);
+            body = body.Replace("{Email}", pv.Email);
+            body = body.Replace("{Phone}", pv.Phone);
+            body = body.Replace("{Office}", pv.Office);
+            body = body.Replace("{Refference}", pv.Refference);
+            body = body.Replace("{Message}", pv.Message);
+
+            using (MailMessage mailMessage = new MailMessage())
+            {
+                mailMessage.From = new MailAddress(pv.Name);
+                mailMessage.Subject = "New User";
+                mailMessage.Body = body;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.To.Add(new MailAddress("sagorkhan.fts@gmail.com"));
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "test.sgff@gmail.com";
+                smtp.EnableSsl = true;
+                System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                NetworkCred.UserName = "test.sgff@gmail.com";
+                NetworkCred.Password = "testingg";
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 587;
+                smtp.Send(mailMessage);
+            }
+
+            return View("~/Views/Contact/Index.cshtml", pv);*/
+
 
             try
             {
